da0450cce88ab1e524811a0ab268fbb0b6e70ec8
N-hikaru <nh930928@icloud.com>
2020-11-24 07:47:56 +0900

    ID検索

--- hello/views.py
+++ hello/views.py
@@ -1,24 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from .models import Friend
 from .forms import HelloForm
 
-class HelloView(TemplateView):
 
-    def __init__(self):
-        self.params = {
-            'title':'Hello',
-            'form': HelloForm(),
-            'result': None
-        }
+def index(request):
+    params = {
+        'title':'Hello',
+        'message': 'all friends',
+        'form': HelloForm(),
+        'data': []        
+    }
 
-    def get(self, request):
-        return render(request, 'hello/index.html', self.params)
-
-    def post(self, request):
-        if('check' in request.POST):
-            self.params['result'] = 'Checked!!'
-        else:
-            self.params['result'] = 'not Checked...'
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'hello/index.html', self.params)        
+    if (request.method == 'POST'):
+        num = request.POST['id']
+        item = Friend.objects.get(id=num)
+        params['data'] = [item]
+        params['from'] = HelloForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all()
+    return render(request, 'hello/index.html', params)
+    
